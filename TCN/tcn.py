04f342dc5b1eeb5e3843e0a8e1b5a6a2828a7d13
Alind Khare <alindkhare@gatech.edu>
2022-09-14 11:32:36 -0400

    Minimal change check

--- TCN/tcn.py
+++ TCN/tcn.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 from torch.nn.utils import weight_norm
-
+from torch import _weight_norm
+import torch.nn.functional as F
 
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):
@@ -44,6 +45,65 @@ class TemporalBlock(nn.Module):
         res = x if self.downsample is None else self.downsample(x)
         return self.relu(out + res)
 
+class ElaticTemporalBlock1(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        # self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+        #                          self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+    
+    def apply_conv(self, conv_layer, x):
+        filters = self.get_active_filter(conv_layer)
+        F.conv1d(
+            x,
+            filters,
+            conv_layer.bias,
+            conv_layer.stride,
+            conv_layer.padding,
+            conv_layer.dilation,
+            conv_layer.groups,
+        )
+    def get_active_filter(self, conv_layer):
+        return _weight_norm(
+            conv_layer.weight_v,
+            conv_layer.weight_g,
+            0,
+        )
+
+    def forward(self, x):
+        # out = self.net(x)
+        # out = x
+        out = self.apply_conv(self.conv1, x)
+        out = self.chomp1(out)
+        out = self.relu1(out)
+        out = self.dropout1(out)
+
+        out = self.apply_conv(self.conv2, out)
+        out = self.chomp2(out)
+        out = self.relu2(out)
+        out = self.dropout2(out)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
 
 class TemporalConvNet(nn.Module):
     def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
@@ -54,7 +114,7 @@ class TemporalConvNet(nn.Module):
             dilation_size = 2 ** i
             in_channels = num_inputs if i == 0 else num_channels[i-1]
             out_channels = num_channels[i]
-            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+            layers += [ElaticTemporalBlock1(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
                                      padding=(kernel_size-1) * dilation_size, dropout=dropout)]
 
         self.network = nn.Sequential(*layers)
